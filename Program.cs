e9e3bb7973d06ec59faf6df901d3daaa239bbda8
AdrianVillodres <adrian.villodres.7e8@itb.cat>
2025-11-06 11:38:04 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ExamenProgramacio
+{
+    public class ExamenUF1
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
