ffe6ad481645eff028af1a5a221f88d9b90280aa
AdrianVillodres <adrian.villodres.7e8@itb.cat>
2025-11-06 12:43:51 +0100

    code first verison done

--- Program.cs
+++ Program.cs
@@ -6,66 +6,66 @@ namespace ExamenProgramacio
     {
         public static void Main()
         {
-            const string INTROMSG = "Give me a number(0 if you want to exit): ";
-            const string NUMMSG = "The numbers that are divisors of {0} are: ";
-            const string ERRORMSG = "Error, give me a integer number(n > 0) or 0";
-            const string OUTPUTMSG = "{0} ";
-            const string ZEROMSG = "Programa finalitzat";
+            
+            const string INTROMSG = "Introduce the secret code for level: ";
+            const string GENDORMSG = "{0} door generated";
+            const string DOORSTOOPEN = "You can open the doors: ";
+            const string DOORSMSG = "{0} ";
+            const string ERRORMSG = "Error, give me a integer number(n > 0)";
 
             int num;
             int divNum;
             bool isValid;
+            Random rand = new Random();
+            int randNum;
 
             isValid = true;
-            num = 1;
-            while(num != 0)
+            num = 0;
+            Console.WriteLine(INTROMSG);
+            try
             {
-                Console.WriteLine(INTROMSG);
-                try
-                {
-                    num = Int32.Parse(Console.ReadLine());
-                }
-                catch (OverflowException)
-                {
-                    Console.WriteLine(ERRORMSG);
-                    isValid = false;
-                }
-                catch (FormatException)
-                {
-                    Console.WriteLine(ERRORMSG);
-                    isValid = false;
-                }
-                catch (Exception)
-                {
-                    Console.WriteLine(ERRORMSG);
-                    isValid = false;
-                }
-
-                if (isValid)
-                {
-                    if(num > 0)
-                    {
-                        Console.Write(NUMMSG, num);
-                        for (int i = 1; i <= num; i++)
-                        {
-                            if (num % i == 0)
-                            {
-                                Console.Write(OUTPUTMSG, i);
-                            }
-                        }
-                        Console.WriteLine();
-                    }
-                    else if(num == 0)
-                    {
-                        Console.WriteLine(ZEROMSG);
-                    }
-                    else
-                    {
-                        Console.WriteLine(ERRORMSG);
-                    }
-                }
-
+                num = Int32.Parse(Console.ReadLine());
             }
+            catch (OverflowException)
+            {
+                Console.WriteLine(ERRORMSG);
+                isValid = false;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine(ERRORMSG);
+                isValid = false;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine(ERRORMSG);
+                isValid = false;
+            }
+
+            if (isValid)
+            {
+                if (num > 0)
+                {
+                    randNum = rand.Next(5, 16);
+                    Console.WriteLine(GENDORMSG, randNum);
+                    Console.Write(DOORSTOOPEN);
+                    for (int i = 1; i <= randNum; i++)
+                    {
+                        if (i % num == 0)
+                        {
+                            Console.Write(DOORSMSG, i);
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(ERRORMSG);
+                }
+            }
+                
+                    
+                
+          
 
 
             
